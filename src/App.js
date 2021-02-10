2864e1389b5b9323322c691b50efc348f23f23f4
Oliver N <72499511+NemesOliver@users.noreply.github.com>
2021-02-10 14:52:55 +0000

    display item and clear input field

--- src/App.js
+++ src/App.js
@@ -5,44 +5,20 @@ import React, { useState } from "react";
 import Header from "./components/Header";
 import AddItem from "./components/AddItem";
 import ItemList from "./components/ItemList";
-import { uuid } from "uuidv4";
 
 function App() {
   //state
   const [input, setInput] = useState("");
   const [items, setItems] = useState([]);
-  //functions
-  const addToStatehandler = (e) => {
-    e.preventDefault();
-    // push inputs in the array **
-    if (input !== "") {
-      if (items.length === 0) {
-        setItems([
-          {
-            itemValue: input,
-            id: uuid(),
-          },
-        ]);
-      } else {
-        setItems([
-          ...items,
-          {
-            itemValue: input,
-            id: uuid(),
-          },
-        ]);
-      }
-    }
-  };
+
   return (
     <div className="shopping-container">
       <Header />
       <AddItem
-        addToStatehandler={addToStatehandler}
         input={input}
         setInput={setInput}
         setItems={setItems}
-        items={items.itemValue}
+        items={items}
       />
       <ItemList items={items} input={input} setItems={setItems} />
     </div>
--- src/components/AddItem.js
+++ src/components/AddItem.js
@@ -1,13 +1,32 @@
 //add item component
 
-const AddItem = ({ addToStatehandler, setInput }) => {
+//uuid
+import { uuid } from "uuidv4";
+
+const AddItem = ({ input, setInput, items, setItems }) => {
+  //functions
+  const submitHandler = (e) => {
+    e.preventDefault();
+    if (input !== "") {
+      setItems([
+        ...items,
+        {
+          itemValue: input,
+          id: uuid(),
+        },
+      ]);
+    }
+    setInput("");
+  };
+
   return (
-    <form onSubmit={addToStatehandler}>
+    <form onSubmit={submitHandler}>
       <input
         onChange={(e) => setInput(e.target.value)}
         className="userInput"
         type="text"
         placeholder="   Add item..."
+        value={input}
       />
       <button>add +</button>
     </form>
