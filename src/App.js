656545c9c6e279e1956f379101d3459e805eb224
Oliver N <72499511+NemesOliver@users.noreply.github.com>
2021-02-12 16:19:25 +0000

    save to local storage

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 //import state
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 //components
 import Header from "./components/Header";
 import AddItem from "./components/AddItem";
@@ -10,6 +10,18 @@ function App() {
   //state
   const [input, setInput] = useState("");
   const [items, setItems] = useState([]);
+
+  //local storage
+  useEffect(() => {
+    const items = JSON.parse(localStorage.getItem("items"));
+    if (items) {
+      setItems(items);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("items", JSON.stringify(items));
+  }, [items]);
 
   return (
     <div className="shopping-container">
